4f66bba8cad7a3ccdcf950c6fa0809310559bfed
Adriana <adrianasotojimenez3@gmail.com>
2025-05-26 23:26:27 -0400

    feat: add timer to ruby code

--- criba_Eratostenes/criba_Ruby - copia/criba_R.rb
+++ criba_Eratostenes/criba_Ruby - copia/criba_R.rb
@@ -1,4 +1,4 @@
-# Criba de Eratóstenes en Ruby con entrada desde la terminal
+require 'benchmark'
 
 def criba_eratostenes(n)
   primos = Array.new(n + 1, true)
@@ -16,9 +16,14 @@ end
 if __FILE__ == $0
   puts "Ingrese la cantidad:"
   n = gets.to_i
-  lista_primos = criba_eratostenes(n)
+
+  lista_primos = nil
+  tiempo = Benchmark.measure do
+    lista_primos = criba_eratostenes(n)
+  end
 
   puts "\nPrimos hasta #{n}: #{lista_primos.size} encontrados"
   puts "Lista de primos:"
   puts lista_primos.join(", ")
+  puts "\n⏱ Tiempo de ejecución: #{tiempo.real.round(6)} segundos"
 end
