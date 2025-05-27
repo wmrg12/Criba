962c77ecf3ddb8fc27d6fc28882d0b4fa6238d1c
Vicente Guzman Aliza Abigail <alizaabigailvicenteguzman@gmail.com>
2025-05-26 21:35:32 -0400

    renegando por el bendito -

--- criba_Eratostenes/criba_Python - copia/criba_P.py
+++ criba_Eratostenes/criba_Python - copia/criba_P.py
@@ -1,20 +1,29 @@
-# Criba de Eratóstenes en Python con entrada desde la terminal
+import numpy as np
+from numba import njit
+import time
 
-def criba_eratostenes(n):
-    primos = [True] * (n + 1)
-    primos[0] = primos[1] = False
+# Criba de Eratóstenes
+@njit
+def criba_eratostenes_numba(n):
+    primos = np.ones(n + 1, dtype=np.bool_) 
+    primos[0] = False
+    primos[1] = False
     for i in range(2, int(n ** 0.5) + 1):
         if primos[i]:
             for j in range(i * i, n + 1, i):
                 primos[j] = False
-    return [i for i, es_primo in enumerate(primos) if es_primo]
+    return np.where(primos)[0]  
 
 if __name__ == "__main__":
     try:
+        # Medición de tiempo
         n = int(input("Ingrese la cantidad: "))
-        lista_primos = criba_eratostenes(n)
-        print(f"\n Primos hasta {n}: {len(lista_primos)} encontrados")
+        t_inicio = time.time()
+        lista_primos = criba_eratostenes_numba(n)
+        t_fin = time.time()
+        print(f"\nPrimos hasta {n}: {len(lista_primos)} encontrados")
         print("Lista de primos:")
         print(lista_primos)
+        print(f"\nTiempo de ejecución: {t_fin - t_inicio:.6f} segundos")
     except ValueError:
-        print("Por favor ingrese un numero entero válido.")
+        print("Por favor ingrese un número entero válido.")
